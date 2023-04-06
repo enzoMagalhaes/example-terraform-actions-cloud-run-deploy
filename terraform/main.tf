22576a285a984c47de2442da9e1f4c633ab0b418
Enzo Magalhaes <magalhaes.enzo82@gmail.com>
2023-04-06 11:29:25 -0300

    changing bucket name

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "gcs_bucket" {
- name          = "test-bucket"
+ name          = "example-terraform-actions-bucket-22323"
  location      = "US-EAST1"
  storage_class = "STANDARD"
  uniform_bucket_level_access = true
