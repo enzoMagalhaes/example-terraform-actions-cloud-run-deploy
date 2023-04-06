5e0d62981e072b6f62fa1d87f99a2ca59f133f06
Enzo Magalhaes <magalhaes.enzo82@gmail.com>
2023-04-06 15:26:39 -0300

    adding setup-terraform github actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,17 +4,9 @@ provider "google" {
   zone    = var.zone
 }
 
-resource "google_storage_bucket" "gcs_bucket" {
-  name                        = "example-terraform-actions-bucket-22323"
-  location                    = "US-EAST1"
-  storage_class               = "STANDARD"
-  uniform_bucket_level_access = true
-}
-
 resource "google_artifact_registry_repository" "flask_app_container_repo" {
   location      = "us-east1"
-  repository_id = "test_flask_app_container_repo"
+  repository_id = "test-flask-app-container-repo"
   description   = "test cloud run container registry"
   format        = "DOCKER"
-  mode          = "STANDARD"
 }
