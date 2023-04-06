02d58d91dce13265e0dd2c69405ac09214bdce23
Enzo Magalhaes <magalhaes.enzo82@gmail.com>
2023-04-06 14:54:22 -0300

    adding sample app and artifact registry provision

---
+++ app/index.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def run():
+    return "hello world!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=int('3000'),debug=True)
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,20 @@
 provider "google" {
-  project     = var.project
-  region      = var.region
-  zone        = var.zone
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_storage_bucket" "gcs_bucket" {
- name          = "example-terraform-actions-bucket-22323"
- location      = "US-EAST1"
- storage_class = "STANDARD"
- uniform_bucket_level_access = true
+  name                        = "example-terraform-actions-bucket-22323"
+  location                    = "US-EAST1"
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
+}
+
+resource "google_artifact_registry_repository" "flask_app_container_repo" {
+  location      = "us-east1"
+  repository_id = "test_flask_app_container_repo"
+  description   = "test cloud run container registry"
+  format        = "DOCKER"
+  mode          = "STANDARD"
 }
