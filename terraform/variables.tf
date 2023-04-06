b0e74acdbfb4fa7a47bbebea67339868517161ba
Enzo Magalhaes <magalhaes.enzo82@gmail.com>
2023-04-06 11:25:17 -0300

    adding variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+
+variable "project" {
+  type        = string
+  description = "The Google Cloud Project Id"
+}
+
+variable "region" {
+  type        = string
+  description = "this variable contains the region"
+}
+
+variable "zone" {
+  type        = string
+  description = "this variable contains the zone"
+}
