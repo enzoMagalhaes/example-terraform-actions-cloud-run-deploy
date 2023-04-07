62a75b0d0ff996f4fcd0275b8ba245fbdf8a5d07
Enzo Magalhaes <magalhaes.enzo82@gmail.com>
2023-04-06 21:00:13 -0300

    fixing image/repository name

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ provider "google" {
 
 resource "google_artifact_registry_repository" "flask_app_container_repo" {
   location      = "us-east1"
-  repository_id = "test-flask-app-container-repo"
+  repository_id = var.repository_name
   description   = "test cloud run container registry"
   format        = "DOCKER"
 }
